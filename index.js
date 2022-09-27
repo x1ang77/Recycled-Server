88d25286d2e999164666f8c2e3a976a27f9eab07
Xiang Ze <xchuay@gmail.com>
2022-09-27 13:48:47 +0800

    second commit

--- index.js
+++ index.js
@@ -11,15 +11,13 @@ app.use(express.static("public"));
 
 const { PORT, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
-// mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`);
-
 mongoose.connect(
     "mongodb+srv://XiangZe:Mcfc1234@cluster0.lnaghki.mongodb.net/?retryWrites=true&w=majority"
 );
 
-app.use("/users", require("./routes/users"));
-app.use("/deposits", require("./routes/deposits"));
-app.use("/rewards", require("./routes/rewards"));
+app.use("/users", require("./api/users"));
+app.use("/deposits", require("./api/deposits"));
+app.use("/rewards", require("./api/rewards"));
 
 app.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
 mongoose.connection.once("open", () =>
